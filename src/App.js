efb5b5fe176413b4679518570f8a504456237028
developer@trashcash.ph <sacdalanryanchristian6404@gmail.com>
2022-01-11 11:14:04 +0800

    feat: Adding new users

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import { useEffect, useState } from "react";
 import { db } from "./firebase-config";
-import { collection, getDocs } from "firebase/firestore";
+import { collection, getDocs, addDoc } from "firebase/firestore";
 import "./App.css";
 
 function App() {
+  const [newName, setNewName] = useState("");
+  const [newAge, setNewAge] = useState(0);
   const [users, setUsers] = useState([]);
   const userCollectionRef = collection(db, "users");
+
+  const createUser = async () => {
+    await addDoc(userCollectionRef, {
+      name: newName,
+      age: newAge,
+    }).then(() => {
+      setNewName("");
+      setNewAge(0);
+      console.log("New user added!");
+    });
+  };
 
   useEffect(() => {
     const getUsers = async () => {
@@ -18,6 +31,26 @@ function App() {
   return (
     <div className="App">
       <h1>React with firebase crud</h1>
+
+      <input
+        type="text"
+        placeholder="Enter name"
+        onChange={(e) => {
+          setNewName(e.target.value);
+        }}
+        value={newName}
+      />
+      <input
+        type="number"
+        placeholder="Enter age"
+        onChange={(e) => {
+          setNewAge(e.target.value);
+        }}
+        value={newAge}
+      />
+
+      <button onClick={createUser}>Create User</button>
+
       {users.map((user) => {
         return (
           <div key={user.id}>
