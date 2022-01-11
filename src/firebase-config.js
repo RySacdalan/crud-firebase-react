277c8e20a62eb2299e57da9ca03f27705b25b6cc
developer@trashcash.ph <sacdalanryanchristian6404@gmail.com>
2022-01-11 09:30:45 +0800

    feat: setup firebase

---
+++ src/firebase-config.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAIN0rxlaf_KUxrtpMW-Sq2VLmNT0CTEWg",
+  authDomain: "crud-firebase-react-3e1b7.firebaseapp.com",
+  projectId: "crud-firebase-react-3e1b7",
+  storageBucket: "crud-firebase-react-3e1b7.appspot.com",
+  messagingSenderId: "67312670457",
+  appId: "1:67312670457:web:70d567b113a1aa55597428",
+};
+
+//establish a connection to firebase
+//initialization of connection
+const app = initializeApp(firebaseConfig);
